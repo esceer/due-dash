69ff4f9acbff4fc67b410f9717e9c7bfedd7ccc2
Roland Kutas <roland.kutas@dmgmori-heitec.hu>
2026-04-17 23:28:06 +0200

    Refactor the existing code into smaller components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "./core/components/sidebar/sidebar.component";
+import { HeaderComponent } from './core/components/header/header.component';
+import { TaskListComponent } from './features/tasks/components/task-list/task-list.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'dd-root',
+  imports: [RouterOutlet, HeaderComponent, SideBarComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.None
---
+++ frontend/src/app/features/tasks/components/task-list/task-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dd-task-list',
+  imports: [],
+  templateUrl: './task-list.component.html',
+})
+export class TaskListComponent {
+
+}
